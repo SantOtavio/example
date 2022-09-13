37879b39e617b4623781f87c65ca6c33124efb97
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-09-13 14:00:49 -0300

    a
    a

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.querySelector('button').addEventListener('click', function() {
     document.querySelector('h1').style.color = 'red';
     }
+    document.querySelector('h1').style.color = 'blue';
+});
 );
